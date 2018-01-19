3d34ffe3097c21cb06eecfc133279a841b9c8fe4
Michael Gogins <michael.gogins@gmail.com>
2018-01-19 14:01:46 -0500

    Adding dependencies (#2).

--- dependencies/update-dependency-packages.sh
+++ dependencies/update-dependency-packages.sh
@@ -3,6 +3,11 @@ echo "Updating all Debian packages required for csound-extended..."
 echo
 echo "Updating Eigen3..."
 sudo apt-get install libeigen3-dev
-echo "Updating FLTK..."
-sudo apt-get install libfltk1.4-dev
-sudo apt-get install libfltk-images1.4
+echo "Updating FLTK 1.3..."
+sudo apt-get install libfltk1.3-dev
+sudo apt-get install libfltk-images1.3
+echo "Updating PortAudio..."
+sudo apt-get install portaudio19-dev
+echo "Updating PortMidi..."
+sudo apt-get install libportmidi-dev
+echo "Finished updating all Debian packages required for csound-extended."
