6925c5221b951d0287804374c3d44e6f6730a10d
Michael Gogins <michael.gogins@gmail.com>
2018-01-19 14:49:02 -0500

    Adding dependencies (#2).

---
+++ dependencies/update-dependency-downloads.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Updating all direct downloads required for csound-extended..."
+echo
+echo "Updating ASIO SDK..."
+wget "http://www.steinberg.net/sdk_downloads/asiosdk2.3.zip"
+echo "Updating VST SDK..."
+wget "http://www.steinberg.net/sdk_downloads/vstsdk367_03_03_2017_build_352.zip"
+echo "Finished updating all direct downloads required for csound-extended."
