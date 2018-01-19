3db07b495c09befc289ce6ef43eecd7eb5e961fd
Michael Gogins <michael.gogins@gmail.com>
2018-01-19 11:16:43 -0500

    Directory restructuring (#1).

--- mingw64-linux/build32.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir csound-mingw64-i686
-cd csound-mingw64-i686 
-cmake ../.. -DCMAKE_TOOLCHAIN_FILE=../Toolchain-mingw64-i686.cmake -DCMAKE_INSTALL_PREFIX=dist -DCUSTOM_CMAKE=../Custom-mingw64-i686.cmake
-make -j6 install
-
