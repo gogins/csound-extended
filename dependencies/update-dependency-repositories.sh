cd46fddb6e912442a442299de2e7f9f8ffab4b97
Michael Gogins <michael.gogins@gmail.com>
2018-01-19 22:17:42 -0500

    Cleaning up dependencies scripts (#2).

---
+++ dependencies/update-dependency-repositories.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "Pulling from submodule repositories..."
+cd csound
+echo "Pulling from `pwd`..."
+git pull
+cd ..
+cd eigen
+echo "Pulling from `pwd`..."
+git pull
+cd ..
+cd liblo
+echo "Pulling from `pwd`..."
+git pull
+cd ..
+cd libmusicxml
+echo "Pulling from `pwd`..."
+git pull
+cd ..
+cd libsndfile
+echo "Pulling from `pwd`..."
+git pull
+cd ..
+cd link
+echo "Pulling from `pwd`..."
+git pull
+cd ..
+cd luajit-2.0
+echo "Pulling from `pwd`..."
+git pull
+cd ..
+cd oboe
+echo "Pulling from `pwd`..."
+git pull
+cd ..
+cd portaudio
+echo "Pulling from `pwd`..."
+git pull
+cd ..
+cd stk
+echo "Pulling from `pwd`..."
+git pull
+cd ..
+echo "Finished pulling from submodule repositories."
+
+
