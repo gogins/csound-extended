61543618a157928890f157866f6731bdfb87743f
gogins <michael.gogins@gmail.com>
2018-01-24 13:32:43 -0500

    Androlid build (#7).

--- CsoundForAndroid/dependencies/update-dependency-repositories.sh
+++ CsoundForAndroid/dependencies/update-dependency-repositories.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 echo "Pulling from submodule repositories for CsoundForAndroid..."
-fluidsynth-android
-liblo-android
-libsndfile-android
-link
-luajit-2.0
-oboe
-stk
+cd fluidsynth-android
+echo "Pulling from `pwd`..."
+git checkout master
+git pull
+cd ..
+cd liblo-android
+echo "Pulling from `pwd`..."
+git checkout master
+git pull
+cd ..
+cd libsndfile-android
+echo "Pulling from `pwd`..."
+git checkout master
+git pull
+cd ..
+cd link
+echo "Pulling from `pwd`..."
+git checkout master
+git pull
+cd ..
+cd luajit-2.0
+echo "Pulling from `pwd`..."
+git checkout v2.1
+git pull
+cd ..
+cd oboe
+echo "Pulling from `pwd`..."
+git checkout master
+git pull
+cd ..
+cd stk
+echo "Pulling from `pwd`..."
+git checkout master
+git pull
+cd ..
 echo "Finished pulling from submodule repositories for CsoundForAndroid."
 
 
