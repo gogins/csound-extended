fee5080f0f61efae68a4b090507bff2167a52eff
Michael Gogins <michael.gogins@gmail.com>
2018-01-26 11:46:15 -0500

    Android build changing directory structure (#7).

--- CsoundForAndroid/ndk-libraries/update-dependency-submodules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-clear 
-echo "Updating all submodules for Csound for Android..."
-git submodule update --init --recursive
-git submodule update --recursive
-git submodule status --recursive
-echo "Finished updating all submodules for Csound for Android."
