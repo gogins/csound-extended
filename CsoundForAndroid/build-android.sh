868eaa63e01e03cc61edf574703a8a9daa8287fc
Michael Gogins <michael.gogins@gmail.com>
2018-01-26 13:58:24 -0500

    Android build changing directory structure (#7).

--- CsoundForAndroid/build-android.sh
+++ CsoundForAndroid/build-android.sh
@@ -1,5 +1,79 @@
 #!/bin/bash
 echo "Building all for Android..."
-mkdir -p build-android
-cd dependencies
+echo
+if [ -z "ANDROID_SDK_ROOT" ]; then
+    echo "ERROR: ANDROID_SDK_ROOT is not set. Please set this variable to point to the root directory of your Android SDK installation to continue.";
+    exit;
+fi
+
+if [ -z "$ANDROID_NDK_ROOT" ]; then
+    echo "ERROR: ANDROID_NDK_ROOT is not set. Please set this variable to point to the root directory of your Android Native Development Kit installation to continue.";
+    exit;
+fi
+
+if [ -z "NDK_MODULE_PATH" ]; then
+    echo "ERROR: NDK_MODULE_PATH is not set. Please set this variable to point to the android/pluginlibs directory of this project to continue.";
+    exit;
+fi
+MACHINE="$(uname -s)"
+case "${MACHINE}" in 
+  MINGW*) NDK_BUILD_CMD=$ANDROID_NDK_ROOT/ndk-build.cmd;;
+  *) NDK_BUILD_CMD="$ANDROID_NDK_ROOT/ndk-build -j6"
+esac
+echo "NDK_BUILD_COMMAND: $NDK_BUILD_CMD"
+echo
+
+cd ndk-modules
+
+echo "Building Oboe audio driver library..."
+cd oboe-csound
+$NDK_BUILD_CMD $1
+if [ $? -eq 0 ]; then
+    echo OK
+else
+    echo "Not building Oboe audio driver library..."
+fi
+cd ..
+
+echo "Building luajit-2.1..."
+cd luajit-2.0
+# The luajit library can't be compiled with the clang NDK, so we cross-compile using gcc.
+# We have to turn large file support OFF.
+# PREFIX and ARM produce directories compatible with ndk-build.
+
+# Build for arm. 
+make clean
+make HOST_CC="gcc -m32" BUILD_MODE=static CROSS=arm-linux-gnueabi- TARGET_CFLAGS="-mcpu=cortex-a8 -mfloat-abi=softfp -fPIC -D_FILE_OFFSET_BITS=32" -j6
+if [ $? -eq 0 ]; then
+    echo OK
+else
+    echo "Not building luaJIT library..."
+fi
+make install PREFIX=`pwd`/jni/local MULTILIB=libs/armeabi-v7a
+if [ $? -eq 0 ]; then
+    echo OK
+else
+    echo "Not building luaJIT library..."
+fi
+# Build for arm64.
+make clean
+make HOST_CC="gcc" BUILD_MODE=static CROSS=aarch64-linux-gnu- TARGET_CFLAGS="-fPIC -D_FILE_OFFSET_BITS=32" -j6
+if [ $? -eq 0 ]; then
+    echo OK
+else
+    echo "Not building luaJIT library..."
+fi
+make install PREFIX=`pwd`/jni/local MULTILIB=libs/arm64-v8a
+if [ $? -eq 0 ]; then
+    echo OK
+else
+    echo "Not building luaJIT library..."
+fi
+# Make certain that LuaCsound links only with the STATIC LuaJIT library.
+find . -name *.so* -delete
+cd ..
+
+
+
+cd ..
 echo "Finished building all for Android."
