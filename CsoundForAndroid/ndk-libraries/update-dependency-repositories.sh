c25da6b8da8c786e3d9ccedc24f6ccff15ca363e
Michael Gogins <michael.gogins@gmail.com>
2018-01-26 17:29:32 -0500

    Android build (#7).

---
+++ CsoundForAndroid/ndk-libraries/update-dependency-repositories.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo "Pulling from submodule repositories..."
+cd fluidsynth-android
+echo "pulling from `pwd`"    
+git checkout master
+git pull
+cd ..
+cd liblo-android
+echo "pulling from `pwd`"    
+git checkout master
+git pull
+cd ..
+cd libsndfile-android
+echo "pulling from `pwd`"    
+git checkout master
+git pull
+cd ..
+cd link
+echo "pulling from `pwd`"    
+git checkout master
+git pull
+cd ..
+cd luajit-2.0
+echo "pulling from `pwd`"    
+git checkout v2.1
+git pull
+cd ..
+cd oboe
+echo "pulling from `pwd`"    
+git checkout master
+git pull
+cd ..
+cd stk
+echo "pulling from `pwd`"    
+git checkout master
+git pull
+cd ..
+echo "Finished pulling from submodule repositories."
