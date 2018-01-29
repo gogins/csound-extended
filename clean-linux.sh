ee68063e71b9b6e6f38c7bbcf95c5ba313b76d6d
Michael Gogins <michael.gogins@gmail.com>
2018-01-29 13:27:56 -0500

    Refactoring Csound for Android app -- minor changes (#8).

--- clean-linux.sh
+++ clean-linux.sh
@@ -3,5 +3,9 @@ cd build-linux
 echo "Cleaning all Linux artifacts from `pwd`..."
 make clean
 rm -R --interactive=once *.*
+cd ..
+cd CsoundHtml5
+make clean
+cd ..
 echo "Finished cleaning all Linux artifacts from `pwd`."
 cd ..
