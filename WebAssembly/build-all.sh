2db77d83afc4961cd397100a08cb99190e7c3d42
gogins <michael.gogins@gmail.com>
2018-02-27 09:57:07 -0300

    Correct WASM build.

--- WebAssembly/build-all.sh
+++ WebAssembly/build-all.sh
@@ -1,14 +1,14 @@
 clear
 echo "Building Csound for WebAssembly..."
 echo "Updating the Emscripten toolchain..."
+pushd `pwd`
 cd ~/emsdk
 ./emsdk install sdk-incoming-64bit binaryen-master-64bit -j1
 ./emsdk activate sdk-incoming-64bit binaryen-master-64bit
 source ./emsdk_env.sh
 export EMSCRIPTEN_ROOT=$EMSCRIPTEN
+popd
 echo "Deleting previous build..."
-cd ~/csound/csound/emscripten
-rm /tmp/emscripten_temp/*.*
 rm -rf build-wasm
 echo "Building libsndfile for WebAssembly..."
 sh download_and_build_libsndfile_wasm.sh
