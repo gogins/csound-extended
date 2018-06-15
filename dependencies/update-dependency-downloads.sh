b22bb570d43fd3e4e774d1291a94ee12b463318c
Michael Gogins <michael.gogins@gmail.com>
2018-06-15 10:33:59 -0400

    Updating submodules.

--- dependencies/update-dependency-downloads.sh
+++ dependencies/update-dependency-downloads.sh
@@ -7,9 +7,6 @@ wget "http://www.steinberg.net/sdk_downloads/asiosdk2.3.zip"
 echo "Updating GMM..."
 wget "http://download-mirror.savannah.gnu.org/releases/getfem/stable/gmm-5.1.tar.gz"
 tar xvf gmm-5.1.tar.gz
-echo "Updating VST SDK..."
-wget "http://download.steinberg.net/sdk_downloads/vstsdk368_08_11_2017_build_121.zip"
-7z x -y "vstsdk368_08_11_2017_build_121.zip"
 rm *.zip*
 rm *.gz*
 echo "Finished updating all direct downloads required for csound-extended."
