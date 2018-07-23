ec15ff2f61c92731399e5df5818c97048f7a1545
gogins <michael.gogins@gmail.com>
2018-07-22 21:03:24 -0400

    Added k-means clustering code from genbattle.

--- CsoundAC/MCRM.cpp
+++ CsoundAC/MCRM.cpp
@@ -19,82 +19,83 @@
  */
 #include "CppSound.hpp"
 #include "MCRM.hpp"
+#include "dkm.hpp"
 
 namespace csound
 {
-  MCRM::MCRM() : depth(0)
-  {
-  }
+MCRM::MCRM() : depth(0)
+{
+}
 
-  MCRM::~MCRM()
-  {
-  }
+MCRM::~MCRM()
+{
+}
 
-  void MCRM::setDepth(int depth)
-  {
+void MCRM::setDepth(int depth)
+{
     this->depth = depth;
-  }
+}
 
-  void MCRM::resize(size_t count)
-  {
+void MCRM::resize(size_t count)
+{
     for(size_t i = 0; i < count; i++)
-      {
+    {
         transformations.push_back(createTransform());
-      }
+    }
     weights.resize(transformations.size(), transformations.size());
     for(size_t precursor = 0; precursor < transformations.size(); precursor++)
-      {
+    {
         for(size_t successor = 0; successor < transformations.size(); successor++)
-          {
+        {
             weights(precursor,successor) = 1.0;
-          }
-      }
-  }
+        }
+    }
+}
 
-  void MCRM::setTransformationElement(size_t index, size_t row, size_t column, double value)
-  {
+void MCRM::setTransformationElement(size_t index, size_t row, size_t column, double value)
+{
     transformations[index](row,column) = value;
-  }
+}
 
-  void MCRM::setWeight(size_t precursor, size_t successor, double weight)
-  {
+void MCRM::setWeight(size_t precursor, size_t successor, double weight)
+{
     weights(precursor,successor) = weight;
-  }
+}
 
-  void MCRM::iterate(int d, size_t p, const Event &event, double weight)
-  {
+void MCRM::iterate(int d, size_t p, const Event &event, double weight)
+{
     d--;
     if(d < 0)
-      {
+    {
         double velocity = event.getVelocity() * weight;
         if(velocity > 0.0)
-          {
+        {
             score.push_back(event);
-          }
-      }
+        }
+    }
     else
-      {
+    {
         for(size_t s = 0; s < transformations.size(); s++)
-          {
+        {
             const Eigen::MatrixXd &t = transformations[s];
             Eigen::VectorXd ev = t * event;
             Event e = ev;
             double w = 0.0;
             if(weight == -1.0)
-              {
+            {
                 w = 1.0;
-              }
+            }
             else
-              {
+            {
                 w = weights(p, s) * weight;
-              }
+            }
             iterate(d, s, e, w);
-          }
-      }
-  }
+        }
+    }
+}
 
-  void MCRM::generate()
-  {
+void MCRM::generate()
+{
     Event event;
     event.setStatus(144);
     event.setVelocity(64);
@@ -102,14 +103,116 @@ namespace csound
     event.setDuration(1);
     double weight = -1;
     iterate(depth, 0, event, weight);
-  }
+}
 
-  void MCRM::produceOrTransform(Score &score,
-                                size_t beginAt,
-                                size_t endAt,
-                                const Eigen::MatrixXd &coordinates)
-  {
+void MCRM::produceOrTransform(Score &score,
+                              size_t beginAt,
+                              size_t endAt,
+                              const Eigen::MatrixXd &coordinates)
+{
     generate();
     ScoreNode::produceOrTransform(score, beginAt, endAt, coordinates);
-  }
+}
+
+KMeansMCRM::KMeansMCRM() :
+    algorithm_type(RANDOM),
+    sample_count(2000000),
+    means_count(2000)
+{
+}
+
+KMeansMCRM::~KMeansMCRM()
+{
+}
+
+void KMeansMCRM::generate()
+{
+    if (algorithm_type == RANDOM) {
+        random_algorithm();
+    } else if (algorithm_type == DETERMINISTIC) {
+        deterministic_algorithm();
+    }
+    means_to_notes();
+}
+
+void KMeansMCRM::random_algorithm()
+{
+}
+
+void KMeansMCRM::iterate(int d, size_t p, const Event &event, double weight)
+{
+    d--;
+    if (samples.size() >= sample_count) {
+        return;
+    }
+    if(d < 0)
+    {
+        double velocity = event.getVelocity() * weight;
+        if(velocity > 0.0)
+        {
+            samples.push_back({event[0], event[1], event[3], event[4], event[5], event[6]});
+        }
+    }
+    else
+    {
+        for(size_t s = 0; s < transformations.size(); s++)
+        {
+            const Eigen::MatrixXd &t = transformations[s];
+            Eigen::VectorXd ev = t * event;
+            Event e = ev;
+            double w = 0.0;
+            if(weight == -1.0)
+            {
+                w = 1.0;
+            }
+            else
+            {
+                w = weights(p, s) * weight;
+            }
+            iterate(d, s, e, w);
+        }
+    }
+}
+
+
+void KMeansMCRM::deterministic_algorithm()
+{
+    System::inform("KMeansMCRM::deterministic_algorithm...\n");
+    Event event;
+    event.setStatus(144);
+    event.setVelocity(64);
+    event.setKey(60);
+    event.setDuration(1);
+    double weight = -1;
+    iterate(depth, 0, event, weight);
+    System::inform("KMeansMCRM::deterministic_algorithm.\n");
+}
+
+static Event mean_to_note(const std::array<float, KMeansMCRM::MEASURE_DIMENSIONS> &mean)
+{
+    Event event;
+    event.setTime(mean[0]);
+    event.setDuration(mean[1]);
+    event.setStatus(144);
+    event.setInstrument(mean[2]);
+    event.setKey(mean[3]);
+    event.setVelocity(mean[4]);
+    event.setPan(mean[5]);
+    return event;
+}
+
+void KMeansMCRM::means_to_notes()
+{
+    System::inform("KMeansMCRM::means_to_notes...\n");
+    System::inform("dkm::kmeans_lloyd...\n");
+    auto clusters = dkm::kmeans_lloyd(samples, MEASURE_DIMENSIONS);
+    auto &means = std::get<0>(clusters);
+    score.clear();
+    for (size_t i = 0, n = means.size(); i < n; ++i) {
+        score.append(mean_to_note(means[i]));
+    }
+    System::inform("dkm::kmeans_lloyd.\n");
+    System::inform("KMeansMCRM::means_to_notes.\n");
+}
+
 }
--- CsoundAC/MCRM.hpp
+++ CsoundAC/MCRM.hpp
@@ -22,42 +22,90 @@
 #include "Platform.hpp"
 #ifdef SWIG
 %module CsoundAC
-%include "std_string.i"
-%include "std_vector.i"
-%{
+    %include "std_string.i"
+    %include "std_vector.i"
+    %{
 #include "Silence.hpp"
 #include <vector>
-%}
+    %}
 #else
 #include "Silence.hpp"
+#include <vector>
 #endif
 
-namespace csound
-{
-  class SILENCE_PUBLIC MCRM :
-    public ScoreNode
-  {
-    // Hutchinson operator.
-    std::vector< Eigen::MatrixXd > transformations;
-    // Pseudo-Markov operator.
-    Eigen::MatrixXd weights;
-    // Depth of recursion.
-    int depth;
-    // Recursive iteration.
-    void iterate(int depth, size_t p, const Event &event, double weight);
-  public:
-    MCRM();
-    virtual ~MCRM();
-    void setDepth(int depth);
-    void resize(size_t transformations);
-    void setTransformationElement(size_t index, size_t row, size_t column, double value);
-    void setWeight(size_t precursor, size_t successor, double weight);
-    void generate();
-    // Node overrides.
-    virtual void produceOrTransform(Score &score,
-                                    size_t beginAt,
-                                    size_t endAt,
-                                    const Eigen::MatrixXd &coordinates);
-  };
-}
+    namespace csound
+    {
+    class SILENCE_PUBLIC MCRM :
+        public ScoreNode
+    {
+    protected:
+        // Hutchinson operator.
+        std::vector< Eigen::MatrixXd > transformations;
+        // Pseudo-Markov operator.
+        Eigen::MatrixXd weights;
+        // Depth of recursion.
+        int depth;
+        // Recursive iteration.
+        virtual void iterate(int depth, size_t p, const Event &event, double weight);
+    public:
+        MCRM();
+        virtual ~MCRM();
+        void setDepth(int depth);
+        void resize(size_t transformations);
+        void setTransformationElement(size_t index, size_t row, size_t column, double value);
+        void setWeight(size_t precursor, size_t successor, double weight);
+        virtual void generate();
+        // Node overrides.
+        virtual void produceOrTransform(Score &score,
+                                        size_t beginAt,
+                                        size_t endAt,
+                                        const Eigen::MatrixXd &coordinates);
+    };
+
+    /**
+     * Uses k-means clustering to translate the accumulated samples that 
+     * approximate the measure on the iterated function system implemented 
+     * by the multiple copy reducing machine algorithm into a specified 
+     * number of notes.
+     */
+    class SILENCE_PUBLIC KMeansMCRM :
+        public MCRM
+    {
+    public:
+        /**
+         * The type of algorithm used.
+         */
+        typedef enum {
+            RANDOM = 1,
+            DETERMINISTIC
+        } ALGORITHM_TYPE;
+        ALGORITHM_TYPE algorithm_type;
+        enum {
+            // time, duration, channel, key, velocity, pan
+            MEASURE_DIMENSIONS=6
+        };
+        /**
+         * The number of times a sample is to be generated,
+         * whether randomly or deterministically.
+         */
+        size_t sample_count;
+        /**
+         * The same as k: the number of centroids or means to be computed
+         * from the samples, and later translated to notes.
+         */
+        size_t means_count;
+        /**
+         * The accumulated samples that approximate the measure of the IFS.
+         */
+        std::vector< std::array<float, MEASURE_DIMENSIONS> > samples;
+        KMeansMCRM();
+        virtual ~KMeansMCRM();
+        virtual void iterate(int depth, size_t p, const Event &event, double weight);
+        virtual void generate();
+        virtual void random_algorithm();
+        virtual void deterministic_algorithm();
+        virtual void means_to_notes();
+    };
+
+    }
 #endif
