a9e63971b4ee4c8cd3e770713b76f431866ebb47
gogins <michael.gogins@gmail.com>
2018-08-25 15:15:45 -0400

    Cleaning up cm2 with csound integration.

--- nudruz/load-nudruz.lisp
+++ nudruz/load-nudruz.lisp
@@ -1,70 +1,64 @@
+;;; This file loads nudruz and all of its dependencies, including Common Music 
+;;; and Csound, into Lisp. Some of these systems can be installed using 
+;;; quicklisp, others as Linux packages, others by cloning a git repository, 
+;;; others by downloading an archive, and some are git submodules in the 
+;;; csound-extended repository. Systems not currently working with nudruz are 
+;;; commented out. ALL systems, including nudruz itself, should have symlinks 
+;;; in ~/.local/share/common-lisp/source. 
+;;;
+;;; NOTA BENE: If you can't or won't install one of the systems, comment out 
+;;; the load form here, and comment out code that depends on that system 
+;;; in the source code (especially in nudruz.asd).
+
 #-quicklisp
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
                                        (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
     
-; These are dependencies that can be satisfied by using quicklisp;
-; they should get symlinks to package names in ~/.local/share/common-lisp/source. 
-;alexandria-20170830-git
-;babel-20171227-git
-;bordeaux-threads-v0.8.6
-;cffi_0.19.0
-;cl-heredoc-20101006-git
-;screamer-20150709-git
-;trivial-features-20161204-git
-    
 (require 'asdf)    
-(format t "ASDF version: ~D~%" (asdf:asdf-version))
+(print (format t "ASDF version: ~D~%" (asdf:asdf-version)))
 #+sbcl
 (require 'sb-introspect)
-#+lispworks
-(push "/home/mkg/.local/share/common-lisp/source/" asdf:*central-registry*)
-
-
-;;; Load the Common Foreign Function Interface.
-;(ql:quickload "cffi")
-#-lispworks
-(asdf:load-system :cffi)
-
-;;; Load the cl-heredoc library, which is used for embedding arbitrary 
-;;; Csound code, including quotes and escapes, into Lisp code.
-;(ql:quickload "cl-heredoc")
-(asdf:load-system :cl-heredoc)
-
-;;; Load Bordeaux threads.
-;(ql:quickload "bordeaux-threads")
-(asdf:load-system :bordeaux-threads)
-
-;;; Load screamer for constraint programming.
-;(ql:quickload "screamer")
-(asdf:load-system :screamer)
-
 ;;; Turn off all those zillions of SBCL warnings.
 (declaim #+sbcl (sb-ext:muffle-conditions style-warning))
 (declaim #+sbcl (sb-ext:muffle-conditions sb-ext:compiler-note))
+#+lispworks
+(push "~/.local/share/common-lisp/source/" asdf:*central-registry*)
+(print (format t "ASDF central registry: ~D~%" asdf:*central-registry*))
 
-;;; Load Common Music.
-;;; NOTA BENE: _SYSTEM_ "cm2" defines _PACKAGE_ "cm" (or :cm).
-;(push "/home/mkg/common-music-git/cm2/" asdf:*central-registry*)
-(asdf:load-system :cm2)
-
-;;; Load Csound's ffi wrappers.
-(push "/home/mkg/csound-extended/dependencies/csound/interfaces/" asdf:*central-registry*)
+(asdf:load-system :alexandria)
+(asdf:load-system :babel)
+(asdf:load-system :bordeaux-threads)
+; (asdf:load-system :bpm)
+#-lispworks
+(asdf:load-system :cffi)
+(asdf:load-system :cl-heredoc)
+; (asdf:load-system :cl-variates)
+; (asdf:load-system :clm)
+; clocc must be loaded as clocc.fasl, it doesn't have an asd file.
+(defparameter *clocc-home* #P"~/.local/share/common-lisp/source/clocc/")
+(load (merge-pathnames "clocc.fasl" *clocc-home*))
+; (asdf:load-system :cm-gtk)
+; Available as part of extended-csound.
+(asdf:load-system :cm2) 
+; (asdf:load-system :cmn)
+; Available as part of extended-csound.
 (asdf:load-system :csound)
-(asdf:load-system :sb-csound)
-
-;;; Load CLOCC.
-;;; I can't get it to load using asdf.
-(defparameter *clocc-home* #P"/home/mkg/csound-extended/dependencies/clocc/")
-(push *clocc-home* asdf:*central-registry*)
-(load #P"/home/mkg/csound-extended/dependencies/clocc/clocc.fasl")
-
-;;; Load Drew Krause's code.
-;;; I have moved loading or defining these pre-requisites out of nudruz.lisp 
-;;; and cminit.lisp.
-;;; This one must be installed with: sudo apt-get install cl-rsm-mod
+; (asdf:load-system :rsm-cache)
+; (asdf:load-system :rsm-filter)
+; (asdf:load-system :rsm-gen-prog)
+; (asdf:load-system :rsm-genetic-alg)
 (asdf:load-system :rsm-mod)
-(defparameter *nudruz-home* #P"/home/mkg/csound-extended/nudruz/sources/")
-(push *nudruz-home* asdf:*central-registry*)
+; (asdf:load-system :rsm-modal)
+; (asdf:load-system :rsm-mpoly)
+; (asdf:load-system :rsm-queue)
+; (asdf:load-system :rsm-rand)
+; Available as part of extended-csound.
+(asdf:load-system :sb-csound)
+(asdf:load-system :sb-introspect)
+(asdf:load-system :screamer)
+; (asdf:load-system :series)
+(asdf:load-system :trivial-features)
+
 (asdf:load-system :nudruz)
