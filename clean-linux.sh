bb5df1f57e67cb5af51d44ebb54a1f5e5f75fcfb
gogins <michael.gogins@gmail.com>
2018-12-28 21:43:42 -0500

    Finally got clean right I think.

--- clean-linux.sh
+++ clean-linux.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-if [ -d "$build-linux" ]; then
-    cd build-linux
-    echo "Cleaning all Linux artifacts from `pwd`..."
-    make clean
-    rm -rf --interactive=once *
-    cd ..
-    cd CsoundHtml5
-    make clean
-    rm CsoundHtml5
-    cd ..
-    echo "Finished cleaning all Linux artifacts from `pwd`."
-    cd ..
-fi
+echo "Began cleaning all Linux artifacts from `pwd`...."
+sudo rm -rfd build-linux
+cd CsoundHtml5
+make clean
+rm CsoundHtml5
+cd ..
+echo "Finished cleaning all Linux artifacts from `pwd`."
