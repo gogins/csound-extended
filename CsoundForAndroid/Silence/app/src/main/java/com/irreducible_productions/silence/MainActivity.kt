6871e964263b0a36fbf1fbc3f0b9b10b89bf6895
gogins <michael.gogins@gmail.com>
2019-03-03 09:37:22 +1100

    Redoing Csound for Android as new app (#73).

---
+++ CsoundForAndroid/Silence/app/src/main/java/com/irreducible_productions/silence/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.irreducible_productions.silence
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
