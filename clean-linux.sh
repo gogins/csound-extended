755ed6d9e2858aca1a6c1deacfad878aca5c4b56
gogins <michael.gogins@gmail.com>
2019-05-24 19:11:49 -0400

    Cleanup.

--- clean-linux.sh
+++ clean-linux.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 echo "Began cleaning all Linux artifacts from `pwd`...."
 sudo rm -rfd build-linux
-cd CsoundHtml5
-make clean
-rm CsoundHtml5
-cd ..
 echo "Finished cleaning all Linux artifacts from `pwd`."
