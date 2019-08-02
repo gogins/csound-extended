8596f21d9430695688309482a554a01fe10bbf9d
gogins <michael.gogins@gmail.com>
2019-08-02 14:47:23 -0400

    Cleaning up unused JavaScript dependencies (#99).

--- dependencies/update-dependency-cdns.sh
+++ dependencies/update-dependency-cdns.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Updating all JavaScript dependencies that come from CDNs..."
-cd silencio/js
+cd ../silencio/js
 ls -ll *.js
-cd ../..
+cd ../../dependencies
 echo "Finished updating all JavaScript dependencies that come from CDNs."
