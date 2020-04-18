8007eee6eebab9f83573106a6eaedc368ebb66ed
Michael Gogins <michael.gogins@gmail.com>
2020-04-18 17:38:52 -0400

    No comment.

--- WebAssembly/release-wasm.sh
+++ WebAssembly/release-wasm.sh
@@ -8,9 +8,7 @@ export RELEASE_DIR=csound-extended-wasm-${CSOUND_AUDIO_NODE_VERSION}
 find . -name "*~" -exec rm {} \;
 rm -rf dist-wasm
 mkdir dist-wasm
-###cp build-wasm/libcsound.js dist-wasm/
 cp src/*.js dist-wasm/
-###cp build-wasm/csound_extended.* dist-wasm/
 cp build-wasm/CsoundAudio*.* dist-wasm/
 cp build-wasm/csound_samples.* dist-wasm/
 rm -rf $RELEASE_DIR
@@ -21,6 +19,7 @@ cp -R dist-wasm/* examples
 cp -R examples/* ${RELEASE_DIR}/
 cp -R examples/* ../docs/
 cp -f src/csound_loader.js ../silencio/js/
+cp -R dist-wasm/* ../docs/html/
 
 zip -r ${RELEASE_DIR}.zip ${RELEASE_DIR}
 ls -ll examples/
