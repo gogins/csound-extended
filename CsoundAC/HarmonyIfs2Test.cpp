6e127307f1cf5431db3c82fb0065307d618603d2
Michael Gogins <michael.gogins@gmail.com>
2021-05-11 07:37:08 -0400

    Seems to be working (#157).

--- CsoundAC/HarmonyIfs2Test.cpp
+++ CsoundAC/HarmonyIfs2Test.cpp
@@ -25,16 +25,18 @@ int main(int argc, const char **argv)
     harmony_ifs.initialize(5 , 60., 30., .05, true, true, 1.);
     auto tonic = csound::chordForName("CM9");
     auto subdominant = csound::chordForName("Dm9");
+    subdominant = csound::octavewiseRevoicing(subdominant, 30, 60);
     auto dominant = csound::chordForName("G9");
+    dominant = csound::octavewiseRevoicing(dominant, 28, 60);
     csound::System::message("I:\n%s\n", tonic.information_sector(0).c_str());
     csound::System::message("ii:\n%s\n", subdominant.information_sector(0).c_str());
     csound::System::message("V:\n%s\n", dominant.information_sector(0).c_str());
     ///auto &score = harmony_ifs.getScore();
     //                                                              1    2    3    4    5    6    7    8    9    10   11   12   13   14   15   16
     harmony_ifs.add_interpolation_point_as_chord(  0., tonic,       .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01);
-    harmony_ifs.add_interpolation_point_as_chord(100., subdominant, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01);
-    harmony_ifs.add_interpolation_point_as_chord(220., dominant,    .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01);
-    harmony_ifs.add_interpolation_point_as_chord(300., tonic,       .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01);
+    harmony_ifs.add_interpolation_point_as_chord(100., subdominant, .01, .01, .01, .20, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01);
+    harmony_ifs.add_interpolation_point_as_chord(220., dominant,    .01, .01, .01, .01, .01, .01, .01, .01, .01, .05, .01, .01, .01, .01, .01, .01);
+    harmony_ifs.add_interpolation_point_as_chord(300., tonic,       .05, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01, .01);
     harmony_ifs.initialize_hutchinson_operator();
     double A = 5.13 * M_PI / 180.;
     csound::System::message("A: %9.4f\n", A);
