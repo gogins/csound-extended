6aee9b5e9e342a4a9d4caf4dcc197147e8ec5c35
Michael Gogins <michael.gogins@gmail.com>
2021-07-07 23:02:51 -0400

    Theming.

--- playpen/playpen.py
+++ playpen/playpen.py
@@ -32,7 +32,7 @@ from gi.repository import GLib
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk 
 settings = Gtk.Settings.get_default()
-settings.set_property("gtk-theme-name", "Adwaita")
+settings.set_property("gtk-theme-name", "Yaru")
 settings.set_property("gtk-application-prefer-dark-theme", True)
 ## gi.require_version("Gst", "1.0")
 ## from gi.repository import Gst 
