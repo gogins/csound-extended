8c8f1c0b5339bbacabe32a33c4995896358dc88f
Michael Gogins <michael.gogins@gmail.com>
2021-07-14 17:35:55 -0400

    Whew... would be nice if there were better docs, native call now working (#165).

--- playpen/build.sh
+++ playpen/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo Building Csound interface for JavaScriptCore...
-swig -c++ -javascript -I/usr/local/include -I/usr/local/include/csound -I/usr/include/webkitgtk-4.0 -v8 -v jsc_csound.i
-g++ -Wno-format-security -fpermissive -fPIC -shared jsc_csound_wrap.cxx -I/usr/include/atk-1.0 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/cairo -I/usr/include/harfbuzz -I/usr/include/pango-1.0 -I/usr/include/libsoup-2.4 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/gtk-3.0 -I/usr/include/webkitgtk-4.0 -I/usr/local/include/csound -lcsound64 -ljavascriptcoregtk-4.0 -lpthread -olibjsc_csound.so
+g++ -Wno-attributes -Wno-format-security -fpermissive -fPIC -shared gjs_csound.cpp -I/usr/include/atk-1.0 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/cairo -I/usr/include/harfbuzz -I/usr/include/pango-1.0 -I/usr/include/libsoup-2.4 -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/gtk-3.0 -I/usr/include/webkitgtk-4.0 -I/usr/local/include/csound -lcsound64 -ljavascriptcoregtk-4.0 -lpthread -olibgjs_csound.so
 echo Finished building Csound interface for JavaScriptCore.
--- playpen/gjs_csound.cpp
+++ playpen/gjs_csound.cpp
@@ -6,9 +6,23 @@
 #  define SILENCE_PUBLIC
 #endif
 
+#include <string>
+#include <csound/csound.h>
+#include <csound/csound.hpp>
+#include <csound/csound_threaded.hpp>
+#include <glib-2.0/glib.h>
+#include <glib-2.0/gmodule.h>
+#include <libsoup-2.4/libsoup/soup.h>
+#include <libsoup-2.4/libsoup/soup-address.h>
+#include <gtk/gtk.h>
+#include <gtk/gtk.h>
+#include <webkit2/webkit-web-extension.h>
+
 /**
- * 
-Wrap a subset of CsoundThreaded as a JavaScriptCore WebExtension.
+ *
+Wrap a subset of CsoundThreaded as a JavaScriptCore WebExtension. The following is the csound.node
+signature of Csound.
+
 int result = csound_.Cleanup();
 int result = csound_.CompileCsd(csd.c_str());
 int result = csound_.CompileCsdText(csd.c_str());
@@ -38,50 +52,85 @@ csound_.SetOutput(filename.c_str(), type.c_str(), format.c_str());
 csound_.SetScorePending(value);
 int result = csound_.Start();
 csound_.Stop();
- */
-class JscCsound {
-protected:
-    CsoundThreaded csound;
-public:
-    JscCsound() {};
-    JscCsound(CSOUND *other) {
-        csound.SetCsound(other);
-    };
-    virtual ~JscCsound() {};
-    virtual int Cleanup() {
-        return csound.Cleanup();
-    };
-    virtual int CompileCsdText(const char *text) {
-        return csound.CompileCsdText(text);
-    };
-};
+*/
+
+static CsoundThreaded csound;
 
 extern "C" {
 
-    bool csound_initialize(JSGlobalContextRef context, JSObjectRef *exports);
+    void destroy_notify_(gpointer data) {
+        printf("destroy_notify: %p\n", data);
+    }
 
-    void SILENCE_PUBLIC web_page_created_callback(WebKitWebExtension *extension,
+    void on_gjs_csound_hello() {
+        std::printf("Hello from Csound!\n");
+    }
+
+    void
+    window_object_cleared_callback (WebKitScriptWorld *world,
+                                    WebKitWebPage     *web_page,
+                                    WebKitFrame       *frame,
+                                    gpointer           user_data) 
+    {
+        JSCContext *js_context = webkit_frame_get_js_context_for_script_world(frame, world);
+        JSCValue *js_global_object = jsc_context_get_global_object(js_context);
+        printf("window_object_cleared_callback: page %ld created for %s\n", webkit_web_page_get_id(web_page),
+               webkit_web_page_get_uri(web_page));
+        // Now we add the Csound API to the context.
+        printf("window_object_cleared_callback: defining gjs_csound_hello...\n");
+        JSCValue *hello = jsc_value_new_function(js_context,
+                          "gjs_csound_hello",
+                          G_CALLBACK(on_gjs_csound_hello),
+                          NULL,
+                          NULL,
+                          G_TYPE_NONE,
+                          0);
+        printf("window_object_cleared_callback: defined gjs_csound_hello: %p\n%s\n", hello, jsc_value_to_json(hello, 4));
+        printf("web_page_created_callback: defined gjs_csound_hello: %p\n%s\n", hello, jsc_value_to_json(hello, 4));
+        auto result = jsc_value_function_call(hello, G_TYPE_NONE);
+        jsc_context_set_value (js_context,
+                       "hello",
+                       hello);    
+        }
+
+    void SILENCE_PUBLIC 
+    web_page_created_callback(WebKitWebExtension *extension,
             WebKitWebPage *web_page,
             const GVariant *user_data)
     {
         printf("web_page_created_callback: page %ld created for %s\n", webkit_web_page_get_id(web_page),
-        webkit_web_page_get_uri(web_page));
+               webkit_web_page_get_uri(web_page));
         WebKitFrame *web_kit_frame = webkit_web_page_get_main_frame(web_page);
         JSCContext *js_context = webkit_frame_get_js_context(web_kit_frame);
         JSCValue *js_global_object = jsc_context_get_global_object(js_context);
-        JSContextRef js_context_ref;
-        JSObjectRef exports;
-        csound_initialize(js_global_object, &exports);
+        printf("web_page_created_callback: js_global_object: %p\n", js_global_object);
+        // Now we add the Csound API to the context.
+        printf("web_page_created_callback: defining gjs_csound_hello...\n");
+        JSCValue *hello = jsc_value_new_function(js_context,
+                          "gjs_csound_hello",
+                          G_CALLBACK(on_gjs_csound_hello),
+                          NULL,
+                          NULL,
+                          G_TYPE_NONE,
+                          0);
+        printf("web_page_created_callback: defined gjs_csound_hello: %p\n%s\n", hello, jsc_value_to_json(hello, 4));
+        auto result = jsc_value_function_call(hello, G_TYPE_NONE);
     }
 
     /**
      * Loads this extension when the page is created.
      */
-    void SILENCE_PUBLIC webkit_web_extension_initialize_with_user_data(WebKitWebExtension *extension, 
-                                                                       GVariant *user_data)
+    void SILENCE_PUBLIC 
+    webkit_web_extension_initialize_with_user_data(WebKitWebExtension *extension,
+            GVariant *user_data)
     {
         printf("webkit_web_extension_initialize: %p user_data: %ld\n", extension, g_variant_get_int64(user_data));
         g_signal_connect (extension, "page-created",
                           G_CALLBACK (web_page_created_callback),
-                          NULL);
+                          user_data);
+        g_signal_connect (webkit_script_world_get_default (),
+                          "window-object-cleared",
+                          G_CALLBACK (window_object_cleared_callback),
+                          user_data);
     }
+};
