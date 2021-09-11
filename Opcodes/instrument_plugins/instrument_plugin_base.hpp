7400bd37901f0e2c5e30edec6001df175b208cfd
Michael Gogins <michael.gogins@gmail.com>
2021-09-11 12:28:56 -0400

    #183.

--- Opcodes/instrument_plugins/instrument_plugin_base.hpp
+++ Opcodes/instrument_plugins/instrument_plugin_base.hpp
@@ -94,59 +94,6 @@ class InstrumentPluginBase
 public:
     int init(CSOUND *csound)
     {
-        return NOTOK;
-    }
-    static int init_(CSOUND *csound, void *opcode)
-    {
-        return reinterpret_cast<T *>(opcode)->init(csound);
-    }
-    int kontrol(CSOUND *csound)
-    {
-        return NOTOK;
-    }
-    static int kontrol_(CSOUND *csound, void *opcode)
-    {
-        return reinterpret_cast<T *>(opcode)->kontrol(csound);
-    }
-     /**
-      This is how to compute audio signals for normal opcodes:
-      (1) Zero all frames from 0 up to but not including Offset.
-      (2) Compute all frames from ksmps_offset up to but not including End.
-      (3) Zero all frames from End up to but not including ksmps.
-      Example from a C opcode:
-      uint32_t offset = p->h.insdshead->ksmps_offset;
-      uint32_t early  = p->h.insdshead->ksmps_no_end;
-      uint32_t n, nsmps = CS_KSMPS;
-      if (UNLIKELY(offset)) memset(p->r, '\0', offset*sizeof(MYFLT));
-      if (UNLIKELY(early)) {
-        nsmps -= early;
-        memset(&p->r[nsmps], '\0', early*sizeof(MYFLT));
-      }
-      for (n = offset; n < nsmps; n++) {
-        input1 = MYFLT2LRND(p->a[n]);
-        p->r[n] = (MYFLT) (input1 >> input2);
-      }
-      So in C++ it should look like this (which is much easier to understand):
-      int frameIndex = 0;
-      for( ; frameIndex < kperiodOffset(); ++frameIndex) {
-          asignal[frameIndex] = 0;
-      }
-      for( ; frameIndex < kperiodEnd(); ++frameIndex) {
-          asignal[frameIndex] = compute();
-      }
-      for( ; frameIndex < ksmps(); ++frameIndex) {
-          asignal[frameIndex] = 0;
-      }
-     */
-     OPDS opds;
-};
-
-template<typename T>
-class OpcodeNoteoffBase
-{
-public:
-    int init(CSOUND *csound)
-    {
         IGN(csound);
         return NOTOK;
     }
@@ -257,7 +204,123 @@ public:
     {
         return reinterpret_cast<T *>(opcode)->noteoff(csound);
     }
+    MYFLT pfield(int i) {
+        auto pfields = (CS_VAR_MEM *)opds.insdshead->p1;
+        // Pfields are 1-based, implicit pfield array is 0-based.
+        auto pfield = pfields[i - 1];
+        return pfield->value;
+    }
+    /**
+     * Verify that the named channel exists as a global variable. If the
+     * global variable is not found, it is created and exported as a 
+     * control channel.
+     */
+    bool ensureChannel(CSOUND *csound, const char *channel_name) {
+        void *global = csound->QueryGlobalVariable(csound, channel_name);
+        if (global == nullptr) {
+            char code_buffer[0x512];
+            snprintf(code_buffer, 0x512, "%s chnexport \"%s\", 3\n", channel_name, channel_name);
+            auto result = csound->EvalCode(csound, code_buffer);
+            return result;
+        } else {
+            return true;
+        }
+    }
+    /**
+     * Receives a k-rate value from the named control channel. If the channel 
+     * does not exist, it is created as a global variable and exported as a 
+     * control channel.
+     */
+    MYFLT receiveK(CSOUND *csound, const char *name) {
+        auto result = ensureChannel(csound, name);
+        int error = 0;
+        auto value = csound->GetControlChannel(csound, name, &error);
+        if (error != OK) {
+            csound->Message(csound, "Failed to get channel value for \"%s\" with error %d\n", error);
+        }
+        return value;
+    }
+    /**
+     * Sends a k-rate value to the named control channel. If the channel does 
+     * not exist, it is created as a global variable and exported as a control 
+     * channel.
+     */
+    void sendK(CSOUND *csound, const char *name, MYFLT k_value) const {
+        auto result = ensureChannel(csound, name);
+        csound->SetControlChannel(csound, name, k_value);
+    }
+    /**
+     * Receives a string value from the named control channel. If the channel 
+     * does not exist, it is created as a global variable and exported as a 
+     * control channel. Calling with a size less than 0 returns the size 
+     * required for the buffer, which must be pre-allocated. The value is 
+     * copied into the buffer, and the actual length of the value is returned.
+     */
+    int receiveS(CSOUND *csound, const char *name, char *buffer, int size) {
+        auto result = ensureChannel(csound, name);
+        if (size < 0) {
+            auto size = csound->GetChannelDatasize(csound);
+            return size;
+        }
+        csound->GetStringChannel(csound, name, buffer);
+        return std::strnlen(buffer, size);
+    }
+    /** 
+     * Sends a string value to the named control channel. If the channel does
+     * not exist, it is created as a global variable and exported as a control 
+     * channel.
+     */
+    void sendS(CSOUND *csound, const char *name, const char *value) const {
+        ensureChannel(csound, name);
+        csound->SetStringChannel(csound, name, value);
+    }
+    /**
+     * Receives an audio value from the named control channel. If the channel 
+     * does not exist, it is created as a global variable and exported as a 
+     * control channel. The audio value is an array of ksmps MYFLT samples 
+     * and is returned in the argument, which must be preallocated.
+     */
+    void receiveA(CSOUND *csound, const char *name, MYFLT *a_value) {
+        ensureChannel(csound, name);
+        csound->GetAudioChannel(csound, name, a_value);
+    }
+    /**
+     * Sends an audio value to the named control channel. If the channel 
+     * does not exist, it is created as a global variable and exported as a 
+     * control channel. The audio value must be an array of ksmps MYFLT 
+     * samples.
+     */
+    void sendA(CSOUND *csound, const char *name, MYFLT *a_value) const {
+        ensureChannel(csound, name);
+        csound->SetAudioChannel(csound, name, a_value);
+    }
+    /** 
+     * Receives a frame of streaming phase vocoder data from the named 
+     * control channel, e.g. as might be sent from a pvsout opcode. If the 
+     * channel does not exist, it is created as a global variable and 
+     * exported as a control channel. The PVS data is returned in the 
+     * PVSDATEXT argument, which must be preallocated. An error is returned if 
+     * the frame is not compatible with the channel.
+     */
+    int receivePVS(CSOUND *csound, const char *name, PVSDATEXT *frame) {
+        ensureChannel(csound, name);
+        auto result = csound->GetPvsChannel(csound, frame, name);
+        return result;
+    }
+    /** 
+     * Sends a frame of streaming phase vocoder data to the pvsin opcode 
+     * that is using the named control channel. If the channel does not 
+     * exist, it is created as a global variable and exported as a control 
+     * channel. An error is returned if the frame is not compatible with 
+     * the pvsin opcode.
+     */
+    int sendPVS(CSOUND *csound, const char *name, PVSDATEXT *frame) const {
+        ensureChannel(csound, name);
+        auto result = csound->SetPvsChannel(csound, frame, name);
+        return result;
+    }
     OPDS opds;
+    ARRAYDAT *a_output;
 };
 
 };
