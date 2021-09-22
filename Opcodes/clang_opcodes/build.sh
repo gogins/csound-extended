b4218788f4ca4ca6313c211b213e794c05b4399e
Michael Gogins <michael.gogins@gmail.com>
2021-09-21 20:09:53 -0400

    Preparing to move to own repo (#189).

--- Opcodes/clang_opcodes/build.sh
+++ Opcodes/clang_opcodes/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CLANGLIBS2="-lclangTooling -lclangFrontendTool -lclangFrontend -lclangDriver -lclangSerialization -lclangCodeGen -lclangParse -lclangSema -lclangStaticAnalyzerFrontend -lclangStaticAnalyzerCheckers -lclangStaticAnalyzerCore -lclangAnalysis -lclangARCMigrate -lclangRewrite -lclangRewriteFrontend -lclangEdit -lclangAST -lclangASTMatchers -lclangLex -lclangBasic -lclang"
-g++ -v -Xlinker -g -O2 -fPIC -Wl,-export-dynamic -shared `llvm-config --cxxflags --ldflags` -I/usr/local/include -I/usr/local/include/csound clang_orc_opcode.cpp $CLANGLIBS2 `llvm-config --libs --system-libs` -o clang_orc_opcode.so
+clang++-13 -v -Xlinker -g -O2 -fPIC -Wl,-export-dynamic -shared `llvm-config-13 --cxxflags --ldflags` -I/usr/local/include -I/usr/local/include/csound clang_opcodes.cpp $CLANGLIBS2 `llvm-config-13 --libs --system-libs` -o clang_opcodes.so
 ls -ll
-csound --opcode-lib="./clang_orc_opcode.so" clang_hello.csd
+csound --opcode-lib="./clang_opcodes.so" clang_hello.csd
