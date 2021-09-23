03cbf5bc4bf3672d5d22e4f9bd10debc2452fab6
Michael Gogins <michael.gogins@gmail.com>
2021-09-22 21:01:24 -0400

    Moved to own repository (#189).

--- Opcodes/clang_opcodes/build-main.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export CLANGLIBS2="-lclangTooling -lclangFrontendTool -lclangFrontend -lclangDriver -lclangSerialization -lclangCodeGen -lclangParse -lclangSema -lclangStaticAnalyzerFrontend -lclangStaticAnalyzerCheckers -lclangStaticAnalyzerCore -lclangAnalysis -lclangARCMigrate -lclangRewrite -lclangRewriteFrontend -lclangEdit -lclangAST -lclangASTMatchers -lclangLex -lclangBasic -lclang"
-g++ -v -Xlinker -g -O2 -fPIC `llvm-config --cxxflags --ldflags` -I/usr/local/include -I/usr/local/include/csound main.cpp $CLANGLIBS2 `llvm-config --libs --system-libs` -o main
-ls -ll
-./main hello.cxx
--- Opcodes/clang_opcodes/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export CLANGLIBS2="-lclangTooling -lclangFrontendTool -lclangFrontend -lclangDriver -lclangSerialization -lclangCodeGen -lclangParse -lclangSema -lclangStaticAnalyzerFrontend -lclangStaticAnalyzerCheckers -lclangStaticAnalyzerCore -lclangAnalysis -lclangARCMigrate -lclangRewrite -lclangRewriteFrontend -lclangEdit -lclangAST -lclangASTMatchers -lclangLex -lclangBasic -lclang"
-clang++-13 -v -Xlinker -g -O2 -fPIC -Wl,-export-dynamic -shared `llvm-config-13 --cxxflags --ldflags` -I/usr/local/include -I/usr/local/include/csound clang_opcodes.cpp $CLANGLIBS2 `llvm-config-13 --libs --system-libs` -o clang_opcodes.so
-ls -ll
-csound --opcode-lib="./clang_opcodes.so" clang_hello.csd
